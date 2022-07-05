926f31c4fb6136cc07f7247fbe07f550d89c09e1
Bjorn Henrik Formo <bjornhenrikformo@gmail.com>
2022-07-05 13:45:37 +0200

    better approach

---
+++ windows-adk-oscdimg/tools/chocolateyinstall.ps1
@@ -0,0 +1,53 @@
+﻿$KitsRootReg1 = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Kits\Installed Roots'
+$KitsRootReg2 = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots'
+$Key11        = 'KitsRoot11'
+$Key10        = 'KitsRoot10'
+
+# The same Windows ADK may write it's KitsRoot10-key or KitsRoot11-key, telling where the ADK is installed - 
+# to either $KitsRootReg1 or $KitsRootReg2. On Windows Server 2022 it seems to get written to the former, on 
+# Windows 11 the latter. Try until found. 
+
+# The line below doesn't work because of backwards compatibility - the Get-ItemPropertyValue does not exist on test system
+# $KitsRoot = Get-ItemPropertyValue -Path $KitsRootReg1 -Name $Key -ErrorAction SilentlyContinue
+
+$KitsRootKey = Get-Item -Path $KitsRootReg1 -ErrorAction SilentlyContinue
+# ensure the .GetValue() method exists
+if ($KitsRootKey -is [Microsoft.Win32.RegistryKey]) {
+  # try the key 'KitsRoot11'
+  $KitsRoot = $KitsRootKey.GetValue("$Key11")
+  if ($null -eq $KitsRoot) {
+    # try the key 'KitsRoot10'
+    $KitsRoot = $KitsRootKey.GetValue("$Key10")
+  }
+}
+
+# if $KitsRoot is still $null, move on to the wow6432Node key
+if ($null -eq $KitsRoot) {
+  $KitsRootKey = Get-Item -Path $KitsRootReg2 -ErrorAction SilentlyContinue
+  if ($KitsRootKey -is [Microsoft.Win32.RegistryKey]) {
+    # try the key 'KitsRoot11'
+    $KitsRoot = $KitsRootKey.GetValue("$Key11")
+    if ($null -eq $KitsRoot) {
+      # try the key 'KitsRoot10'
+      $KitsRoot = $KitsRootKey.GetValue("$Key10")
+    }
+  }
+}
+
+# all the previous should be able to run without any exceptions being thrown, but
+# if $KitsRoot is still $null, en exception must be thrown at this point
+if ($null -eq $KitsRoot) {
+  throw "Unable to find installation of Windows ADK"
+} 
+else {
+  $OscdimgSourceFolder = Join-Path -Path $KitsRoot -ChildPath "Assessment and Deployment Kit\Deployment Tools\$($env:PROCESSOR_ARCHITECTURE)\Oscdimg"
+  $OscdimgSourcePath = Join-Path -Path $OscdimgSourceFolder -ChildPath 'oscdimg.exe'
+  if (Test-Path -Path $OscdimgSourcePath -ErrorAction SilentlyContinue) {
+    $PathType = [System.EnvironmentVariableTarget]::Machine
+    Write-Host "The path '$OscdimgSourceFolder' will be added to PATH"
+    Install-ChocolateyPath -pathToInstall $OscdimgSourceFolder -pathType $PathType
+  }
+  else {
+    throw "Unable to find '$OscdimgSourcePath'"
+  }
+}
---
+++ windows-adk-oscdimg/tools/chocolateyuninstall.ps1
@@ -0,0 +1,83 @@
+﻿$KitsRootReg1   = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Kits\Installed Roots'
+$KitsRootReg2   = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots'
+$Key11          = 'KitsRoot11'
+$Key10          = 'KitsRoot10'
+
+# The same Windows ADK may write it's KitsRoot11-key or KitsRoot10-key, telling where the ADK is installed - 
+# to either $KitsRootReg1 or $KitsRootReg2. On Windows Server 2022 it seems to get written to the former, on 
+# Windows 11 the latter. Try until found. 
+
+# The line below doesn't work because of backwards compatibility - the Get-ItemPropertyValue cmdlet 
+# was introduced in PowerShell 5, and does not exist on test system
+# $KitsRoot = Get-ItemPropertyValue -Path $KitsRootReg1 -Name $Key -ErrorAction SilentlyContinue
+
+$KitsRootKey = Get-Item -Path $KitsRootReg1 -ErrorAction SilentlyContinue
+# ensure the .GetValue() method exists
+if ($KitsRootKey -is [Microsoft.Win32.RegistryKey]) {
+  # try the key 'KitsRoot11'
+  $KitsRoot = $KitsRootKey.GetValue("$Key11")
+  if ($null -eq $KitsRoot) {
+    # try the key 'KitsRoot10'
+    $KitsRoot = $KitsRootKey.GetValue("$Key10")
+  }
+}
+
+# if $KitsRoot is still $null, move on to the wow6432Node key
+if ($null -eq $KitsRoot) {
+  $KitsRootKey = Get-Item -Path $KitsRootReg2 -ErrorAction SilentlyContinue
+  if ($KitsRootKey -is [Microsoft.Win32.RegistryKey]) {
+    # try the key 'KitsRoot11'
+    $KitsRoot = $KitsRootKey.GetValue("$Key11")
+    if ($null -eq $KitsRoot) {
+      # try the key 'KitsRoot10'
+      $KitsRoot = $KitsRootKey.GetValue("$Key10")
+    }
+  }
+}
+
+if ($null -eq $KitsRoot) {
+  Write-Warning -Message "Unable to find installation of Windows ADK from the registry - trying the standard filesystem locations"
+  
+  $ProgramFilesx86 = ${env:ProgramFiles(x86)}
+  $ProgramFiles = $env:ProgramFiles
+
+  $PossibleADKPath1 = Join-Path -Path $ProgramFilesx86 -ChildPath "Assessment and Deployment Kit\Deployment Tools\$($env:PROCESSOR_ARCHITECTURE)\Oscdimg"
+  $PossibleADKPath2 = Join-Path -Path $ProgramFiles -ChildPath "Assessment and Deployment Kit\Deployment Tools\$($env:PROCESSOR_ARCHITECTURE)\Oscdimg"
+
+  if (Test-Path -Path $PossiblePath1 -ErrorAction Ignore) {
+    [system.string]$ADKPath = $PossibleADKPath1
+  }
+  elseif (Test-Path -Path $PossiblePath2 -ErrorAction Ignore) {
+    [system.string]$ADKPath = $PossibleADKPath2
+  }
+  else {
+    throw "Unable to find the Path to ADK - remove the path manually"
+  }
+}
+else {
+  [system.string]$ADKPath = Join-Path -Path $KitsRoot -ChildPath "Assessment and Deployment Kit\Deployment Tools\$($env:PROCESSOR_ARCHITECTURE)\Oscdimg"
+}
+
+
+# Get the PATH variable
+Update-SessionEnvironment
+$EnvPath = $env:PATH
+if ($EnvPath.ToLower().Contains($ADKPath.ToLower())) {
+
+  # Create a control count. We're removing only one element, 
+  # if for some reason an operation failed, we wanna abort the
+  # the operation
+  $ControlCount = (($envPath.split(';')).count-1)
+
+  $NewPath = $EnvPath.replace("$ADKPath",'')
+  $NewPath = $NewPath.replace(';;',';')
+  
+  # Make sure the new value contains one less element than before
+  if (($NewPath.split(';').count) -eq $ControlCount) {
+    $VariableType = [System.EnvironmentVariableTarget]::Machine
+    Install-ChocolateyEnvironmentVariable -variableName 'Path' -variableValue $NewPath -variableType $VariableType
+  }
+  else {
+    throw "Unable to remove the path of oscdimg.exe from PATH environment variable, remove manually"
+  }
+}
