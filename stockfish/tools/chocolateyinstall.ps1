bcfca8c3d93f4ae6047463d62e5bd781b176736c
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2023-05-27 16:11:01 +0200

    added stockfish

--- stockfish/tools/chocolateyinstall.ps1
+++ stockfish/tools/chocolateyinstall.ps1
@@ -1,14 +1,16 @@
-﻿$StockFishVersion      = '15'
-$StockFishChecksum     = '92C609A1D449BA2C31682E533F633D064774CBE0C71A5CA550108599462B779F'
-$StockFishChecksumType = 'SHA256'
-$StockFishFileBaseName = "stockfish_$($StockFishVersion)_win_x64"
-$StockFishUrl          = "https://stockfishchess.org/files/$StockFishFileBaseName.zip"
-
+﻿$StockFishVersion       = '15.1'
+$StockFishChecksum      = '8C8F76742065A18789B8F5A74561ABCC9F0A0589478607CAA1FAB2450694657B'
+$StockFishChecksumType  = 'SHA256'
+$StockFishFileBaseName  = "stockfish_$($StockFishVersion)_win_x64_avx2"
+$StockFishUrl           = "https://stockfishchess.org/files/$StockFishFileBaseName.zip"
+$ChessEnginesFolder     = Join-Path -Path $env:ProgramFiles -ChildPath 'ChessEngines'
+# StockFish zip-file root contains a single folder with name = $StockFishFileBaseName, so it can be directly unzipped to the ChessEngines folder
 $InstChocoZipPkgParams = @{
     PackageName    = $env:ChocolateyPackageName
-    UnzipLocation  = $env:ProgramFiles 
+    UnzipLocation  = $ChessEnginesFolder 
     Url            = $StockFishUrl
     Checksum       = $StockFishChecksum
     ChecksumType   = $StockFishChecksumType
 }
 Install-ChocolateyZipPackage @InstChocoZipPkgParams
+Write-Host "StockFish installed to '$ChessEnginesFolder'. Note that you may also install the *chess-package* to get a GUI and a collection of chess engines"
--- stockfish/tools/chocolateyuninstall.ps1
+++ stockfish/tools/chocolateyuninstall.ps1
@@ -1,4 +1,8 @@
-﻿$StockFishVersion       = '15'
-$StockFishFileBaseName  = "stockfish_$($StockFishVersion)_win_x64"
-$StockFishInstallFolder = Join-Path -Path $env:ProgramFiles -ChildPath $StockFishFileBaseName
+﻿# version specific vars
+$StockFishVersion       = '15.1'
+$StockFishFileBaseName  = "stockfish_$($StockFishVersion)_win_x64_avx2"
+
+# non-version specific vars
+$ChessEnginesFolder     = Join-Path -Path $env:ProgramFiles -ChildPath 'ChessEngines'
+$StockFishInstallFolder = Join-Path -Path $ChessEnginesFolder -ChildPath $StockFishFileBaseName
 Remove-Item -Path $StockFishInstallFolder.FullName -Recurse -Force | Out-Null
