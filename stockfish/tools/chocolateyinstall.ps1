b8f5947d7d3f229dea06370772ba06995cd3ee43
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2023-05-27 16:33:17 +0200

    tested stockfish

--- stockfish/tools/chocolateyinstall.ps1
+++ stockfish/tools/chocolateyinstall.ps1
@@ -1,5 +1,8 @@
-﻿$StockFishVersion       = '15.1'
+﻿# version specific vars
+$StockFishVersion       = '15.1'
 $StockFishChecksum      = '8C8F76742065A18789B8F5A74561ABCC9F0A0589478607CAA1FAB2450694657B'
+
+# non-version specific vars
 $StockFishChecksumType  = 'SHA256'
 $StockFishFileBaseName  = "stockfish_$($StockFishVersion)_win_x64_avx2"
 $StockFishUrl           = "https://stockfishchess.org/files/$StockFishFileBaseName.zip"
@@ -13,4 +16,4 @@ $InstChocoZipPkgParams = @{
     ChecksumType   = $StockFishChecksumType
 }
 Install-ChocolateyZipPackage @InstChocoZipPkgParams
-Write-Host "StockFish installed to '$ChessEnginesFolder'. Note that you may also install the *chess-package* to get a GUI and a collection of chess engines"
+Write-Host "StockFish installed to '$(Join-Path -Path $ChessEnginesFolder -ChildPath $StockFishFileBaseName)'.`nNote that you may also install 'chess-package' to get a GUI and a collection of chess engines" -ForegroundColor Yellow
--- stockfish/tools/chocolateyuninstall.ps1
+++ stockfish/tools/chocolateyuninstall.ps1
@@ -1,8 +1,19 @@
 ﻿# version specific vars
 $StockFishVersion       = '15.1'
-$StockFishFileBaseName  = "stockfish_$($StockFishVersion)_win_x64_avx2"
 
 # non-version specific vars
+$StockFishFileBaseName  = "stockfish_$($StockFishVersion)_win_x64_avx2"
 $ChessEnginesFolder     = Join-Path -Path $env:ProgramFiles -ChildPath 'ChessEngines'
 $StockFishInstallFolder = Join-Path -Path $ChessEnginesFolder -ChildPath $StockFishFileBaseName
-Remove-Item -Path $StockFishInstallFolder.FullName -Recurse -Force | Out-Null
+if (Get-Item -Path $StockFishInstallFolder -ErrorAction Ignore) {
+    try {
+        Write-Host "Removing the StockFish engine in '$StockFishInstallFolder'..." -ForegroundColor Yellow
+        Remove-Item -Path $StockFishInstallFolder -Recurse -Force | Out-Null
+        #Get-Item -Path $StockFishInstallFolder | Remove-Item -Recurse -Force | Out-Null
+        Write-Host "Removed the StockFish engine in '$StockFishInstallFolder'."
+    }
+    catch {
+        Write-Warning "Failed to remove the StockFish engine from '$StockFishInstallFolder'" -WarningAction Continue
+        exit 1
+    }  
+}
