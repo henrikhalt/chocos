2941e71d9a6b44cce4a88158c5f5f1013f1e3d88
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2024-02-13 01:30:12 +0100

    added lz0

---
+++ lc0-cuda/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿# version specific vars
+$Lc0Version         = '0.30.0'
+$Lc0Checksum        = '49E8E76CD7D8A62172B49BC7BF80BA2B26D07F3D550BFD51F49D6D479DB2CAA5'
+
+# non-version specific vars
+$Lc0ChecksumType    = 'SHA256'
+$Lc0Url             = "https://github.com/LeelaChessZero/lc0/releases/download/v$Lc0Version/lc0-v$Lc0Version-windows-gpu-nvidia-cuda.zip"
+$ChessEnginesFolder = Join-Path -Path $env:ProgramFiles -ChildPath 'ChessEngines'
+$Lc0InstallFolder   = Join-Path -Path $ChessEnginesFolder -ChildPath "lc0_$Lc0Version"
+
+$InstChocoZipPkgParams = @{
+    PackageName    = $env:ChocolateyPackageName
+    UnzipLocation  = $Lc0InstallFolder 
+    Url            = $Lc0Url
+    Checksum       = $Lc0Checksum
+    ChecksumType   = $Lc0ChecksumType
+}
+Install-ChocolateyZipPackage @InstChocoZipPkgParams
+Write-Host "Leela Chess Zero v$Lc0Version installed to '$Lc0InstallFolder'.`nNote that you may install 'chess-package' to get a GUI and a collection of chess engines" -ForegroundColor Yellow
---
+++ lc0-cuda/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿# version specific vars
+$Lc0Version         = '0.30.0'
+
+# non-version specific vars
+$ChessEnginesFolder = Join-Path -Path $env:ProgramFiles -ChildPath 'ChessEngines'
+$Lc0InstallFolder   = Join-Path -Path $ChessEnginesFolder -ChildPath "lc0_$Lc0Version"
+
+if (Get-Item -Path $Lc0InstallFolder -ErrorAction Ignore) {
+    try {
+        Write-Host "Trying to remove Lc0 engine in '$Lc0InstallFolder'..." -ForegroundColor Yellow
+        Remove-Item -Path $Lc0InstallFolder -Recurse -Force | Out-Null
+        Write-Host "Removed Lc0 engine in '$Lc0InstallFolder'."
+    }
+    catch {
+        Write-Warning "Failed to remove the Lc0 engine from '$Lc0InstallFolder'" -WarningAction Continue
+        exit 1
+    }  
+}
