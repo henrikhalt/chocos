ac40d203f9aa7f0f470aca1a7dbfe70b07d45024
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2024-02-13 03:19:21 +0100

    added banksiagui

---
+++ banksiagui/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿# version specific vars
+$BanksiaGuiVersion       = '0.58'
+$BanksiaGuiChecksum      = '6CCBCC79C31229C66BB03B33C3CF1E9853201F0235A99E1570FBA997A0CC95E5'
+
+# non-version specific vars
+$BanksiaGuiChecksumType  = 'SHA256'
+$BanksiaGuiUrl           = "https://banksiagui.com/dl/BanksiaGui-$BanksiaGuiVersion-win64.zip"
+   
+
+# Installation instruction: "To run the app, just download, unzip and click the program. You need a 64-bit OS, Windows, macOS, or Linux."
+$InstChocoZipPkgParams = @{
+    PackageName    = $env:ChocolateyPackageName
+    UnzipLocation  = $env:ProgramFiles 
+    Url            = $BanksiaGuiUrl
+    Checksum       = $BanksiaGuiChecksum
+    ChecksumType   = $BanksiaGuiChecksumType
+}
+Install-ChocolateyZipPackage @InstChocoZipPkgParams
+
+# Create a shortcut in the Start Menu
+$ShortcutTargetFolder = Join-Path -Path $env:ProgramFiles -ChildPath "BanksiaGui-$BanksiaGuiVersion-win64"
+$ShortcutTargetPath   = Join-Path -Path $ShortCutTargetFolder -ChildPath "BanksiaGUI.exe"
+$InstallChocolateyShortcutParams = @{
+    ShortcutFilePath = Join-Path -Path $env:ProgramData -ChildPath "Microsoft\Windows\Start Menu\Programs\BanksiaGui.lnk"
+    TargetPath       = $ShortcutTargetPath
+    IconLocation     = $ShortcutTargetPath
+    Description      = "BanksiaGui is a feature-rich chess GUI that can be used with UCI-compliant chess engines, such as Stockfish and Leela Chess Zero"
+}
+Install-ChocolateyShortcut @InstallChocolateyShortcutParams
---
+++ banksiagui/tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+﻿# version specific vars
+$BanksiaGuiVersion       = '0.58'
+
+# non-version specific vars
+$BanksiaGuiInstallFolder = Join-Path -Path $env:ProgramFiles -ChildPath "BanksiaGui-$BanksiaGuiVersion-win64"
+
+# Remove the installation folder. There is no uninstall - just remove the folder
+if (Get-Item -Path $BanksiaGuiInstallFolder -ErrorAction Ignore) {
+    try {
+        Remove-Item -Path $BanksiaGuiInstallFolder -Recurse -Force | Out-Null
+    }
+    catch {
+        Write-Warning "Failed to remove the BanksiaGui folder: '$BanksiaGuiInstallFolder'" -WarningAction Continue
+        exit 1
+    }  
+}
+
+# Remove the shortcut from the Start Menu
+$ShortcutFilePath = Join-Path -Path $env:ProgramData -ChildPath "Microsoft\Windows\Start Menu\Programs\BanksiaGui.lnk"
+if (Get-Item -Path $ShortcutFilePath -ErrorAction Ignore) {
+    try {
+        Remove-Item -Path $ShortcutFilePath -Force | Out-Null
+    }
+    catch {
+        Write-Warning "Failed to remove the BanksiaGui shortcut '$ShortcutFilePath'" -WarningAction Continue
+        exit 1
+    }  
+}
