71378eb8317cf3c99be5eb7fa723b93abb465071
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2025-02-10 00:44:55 +0100

    uninstall fails, for some reason, i don't see it

--- microsoft-windows-terminal-zip/tools/chocolateyuninstall.ps1
+++ microsoft-windows-terminal-zip/tools/chocolateyuninstall.ps1
@@ -1,9 +1,9 @@
-﻿$WTTargetRootPath = Join-Path -Path $env:ProgramFiles -ChildPath 'WindowsTerminal'
-$ShortcutLink     = Join-Path -Path $env:ProgramData -ChildPath 'Microsoft\Windows\Start Menu\Programs\Windows Terminal.lnk'
+﻿$WTTargetRootPath = Join-Path -Path "$($env:ProgramFiles)" -ChildPath 'WindowsTerminal'
+$ShortcutLink     = Join-Path -Path "$($env:ProgramData)" -ChildPath 'Microsoft\Windows\Start Menu\Programs\Windows Terminal.lnk'
 
-if(Test-Path -Path $WTTargetRootPath -ErrorAction Ignore) {
-  Remove-Item -Path $WTTargetRootPath -Recurse -Force | Out-Null
+if(Test-Path -Path "$WTTargetRootPath" -ErrorAction Ignore) {
+  Remove-Item -Path "$WTTargetRootPath" -Recurse -Force | Out-Null
 }
-if(Test-Path -Path $ShortcutLink -ErrorAction Ignore) {
-  Remove-Item -Path $ShortcutLink -Force | Out-Null
+if(Test-Path -Path "$ShortcutLink" -ErrorAction Ignore) {
+  Remove-Item -Path "$ShortcutLink" -Force | Out-Null
 }
