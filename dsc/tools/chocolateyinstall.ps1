e746c0a6ccce0c2408487f205d7608bbcfa3b48d
BjÃ¸rn Henrik Formo aka databjÃ¸rn <bjornhenrikformo@gmail.com>
2025-05-13 23:42:48 +0200

    bugfix, and requirements from chocolatey. dscv3 was a better name, but how to explain that v3 is not really the version....

---
+++ dsc/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿# variable
+$DSCv3Version = '3.0.2'
+$DSCv3ZipCheckSum = '1D9A4575C3E1609744055D18074DB5146F30CC8F2107C60C154A8997497659F2'
+
+# static
+$ErrorActionPreference = 'Stop';
+$DSCv3InstallPath = Join-Path -Path $env:PROGRAMFILES -ChildPath 'dsc'
+$Url = "https://github.com/PowerShell/DSC/releases/download/v$($DSCv3Version)/DSC-$($DSCv3Version)-x86_64-pc-windows-msvc.zip"
+
+$UnzipArgs = @{
+  PackageName   = $env:ChocolateyPackageName
+  UnzipLocation = $DSCv3InstallPath
+  Url           = $Url
+  Checksum      = $DSCv3ZipCheckSum
+  ChecksumType  = 'sha256'
+  Force         = $true
+}
+Install-ChocolateyZipPackage @UnzipArgs
+
+# add to path as well
+Install-ChocolateyPath -PathToInstall $DSCv3InstallPath -PathType 'Machine'
---
+++ dsc/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+$DSCv3InstallPath = Join-Path -Path $env:PROGRAMFILES -ChildPath 'dsc'
+$RemoveArgs = @{
+  Path = $DSCv3InstallPath
+  Force = $true
+  Recurse = $true
+  ErrorAction = 'Stop'
+}
+Remove-Item @RemoveArgs
+
+#! The uninstall-ChocolateyPath is documented, but not implemented? 
+# Uninstall-ChocolateyPath -PathToUninstall $DSCv3InstallPath -PathType 'Machine'
+
+#! Manual removal from PATH machine variable.
+# Get the Machine Path from the Registry ($env:Path mixes user and machine paths - we don't want to duplicate user paths into machine paths)
+$MachinePath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+
+# Remove the specific path from the Machine Path
+$NewPath = $MachinePath -replace [regex]::escape($DSCv3InstallPath), ''
+
+# Clean up formatting
+$NewPath = ($NewPath -replace ';\\;', ';') -replace ';;', ';' 
+$NewPath = ($NewPath -replace ';\\$', '') -replace ';$', ''  
+$NewPath = ($NewPath -replace '^\\;', '') -replace '^;', ''  
+
+# Set the updated Machine Path
+[System.Environment]::SetEnvironmentVariable("Path", $NewPath, [System.EnvironmentVariableTarget]::Machine)
